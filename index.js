03cc757f01ff2f77faa44013804fc9bdc039d3bb
Sean Aird <seanraird@yahoo.com>
2021-05-28 14:48:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(feet){
+if (feet <= 400){
+   return ('This one is on me!');
+}else if (feet > 2500) { return ('No can do.'); }else if (feet > 2000)
+{return ('I will gladly take your thirty bucks.');
+
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? ('Ok, sounds good.') : ('No go.');
+  
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+
+switch (Tip) {
+  case Tip = 'generous':
+    return ('Thank you so much.');
+    break;
+  case Tip = 'not as generous':
+    return ('Thank you.');
+    break;
+
+  default:
+     return ("Bye.");
+    break;
+}
+  
 }
